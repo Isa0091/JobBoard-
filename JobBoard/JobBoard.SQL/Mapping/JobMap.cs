a98741036bbe4f88b0c26e8b43e181798b2d1a96
Isa0091 <34584720+Isa0091@users.noreply.github.com>
2020-11-17 01:58:43 -0600

    Mapping de tabla job
    se crea el mapping de la tabla en la cual se almacenaran los trabajos

---
+++ JobBoard/JobBoard.SQL/Mapping/JobMap.cs
@@ -0,0 +1,22 @@
+﻿using JobBoard.Core.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobBoard.SQL.Mapping
+{
+    public class JobMap : IEntityTypeConfiguration<Job>
+    {
+        public void Configure(EntityTypeBuilder<Job> b)
+        {
+            b.HasKey(x => x.Codigo);
+            b.Property(x => x.Titulo).HasMaxLength(256).IsRequired();
+            b.Property(x => x.Descripcion).HasMaxLength(1500).IsRequired();
+            b.Property(x => x.FechaExpiracion).IsRequired();
+            b.Property(x => x.FechaIngreso).IsRequired();
+
+        }
+    }
+}
