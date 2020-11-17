03707397490c8a52cfa96aa149bb6388907f2b20
Isa0091 <34584720+Isa0091@users.noreply.github.com>
2020-11-17 01:52:26 -0600

    Manejop de exceptios
    Se define la clase para el manejo de excepciones

---
+++ JobBoard/JobBoard.Core/Exceptions/JobException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobBoard.Core.Exceptions
+{
+    /// <summary>
+    /// Maneja las excepciones
+    /// </summary>
+    public class JobException : Exception
+    {
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="tipoError"></param>
+        /// <param name="codigoError"></param>
+        /// <param name="mensaje"></param>
+        public JobException(TipoErrorJob tipoError, string mensaje) : base(mensaje)
+        {
+            TipoError = tipoError;
+        }
+
+        /// <summary>
+        /// Tipo de error retornado
+        /// </summary>
+        public TipoErrorJob TipoError { get; set; }
+
+        /// <summary>
+        ///  Tipos de errores 
+        /// </summary>
+        public enum TipoErrorJob
+        {
+            /// <summary>
+            /// El elemento al que se desea acceder no existe
+            /// </summary>
+            NoExiste,
+
+        }
+    }
+}
